6ddb43c3f0c4656377c72a3c44fad9ea99f0ac1c
Aurielle Li <89785862+yuiyeyo@users.noreply.github.com>
2024-01-26 20:26:02 +0700

    Update routes.py

--- user/routes.py
+++ user/routes.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from app import app
-from .models import User
+from user.models import User
 
-@app.route('/user/signup', methods=['GET'])
+@app.route('/user/signup', methods=['POST'])
 def signup():
     return User().signup()
