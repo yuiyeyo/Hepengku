402a101a5b8b9f8c0217088a9f8639f803d867c2
Aurielle Li <89785862+yuiyeyo@users.noreply.github.com>
2024-01-26 20:22:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
+import pymongo
 
 app = Flask(__name__)
+
+client = pymongo.MongoClient('localhost', 27017)
+db = client.hepengku
 
 #Routes
 from user import routes
@@ -9,16 +13,16 @@ from user import routes
 def home():
     return render_template('home.html')
 
-#@app.route('/join')
-#def signup():
-#    return render_template('signup.html')
+@app.route('/join')
+def join():
+    return render_template('signup.html')
 
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
 
-@app.route('/user/signup', methods=['GET'])
-def signup_route():
+@app.route('/user/signup', methods=['POST'])
+def signup_routes():
     return signup()
 
 if __name__ == '__main__':
