9f70d53a9cac326a163c8787eaab6b6e2bc605e8
Aurielle Li <89785862+yuiyeyo@users.noreply.github.com>
2024-01-27 16:16:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, session
+from functools import wraps
 import pymongo
 
 app = Flask(__name__)
+app.secret_key = b'\xcb\xc1\xf98\x7f\xa5\xa5/\xe22\xb7\x19\x0bb\x8bL'
 
 client = pymongo.MongoClient('localhost', 27017)
 db = client.hepengku
+
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            return redirect('/')
+        
+    return wrap
 
 #Routes
 from user import routes
@@ -18,6 +30,7 @@ def join():
     return render_template('signup.html')
 
 @app.route('/dashboard')
+@login_required
 def dashboard():
     return render_template('dashboard.html')
 
@@ -25,6 +38,14 @@ def dashboard():
 def signup_routes():
     return signup()
 
+@app.route ('/user/signout')
+def signout_routes():
+    return signout()
+
+@app.route ('/user/signin', methods = ['POST'])
+def signin_routes():
+    return signin()
+
 if __name__ == '__main__':
-    from user.routes import signup
+    from user.routes import signup, signout, signin
     app.run(debug=True)
