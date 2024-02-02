5d74d19b218dc2e6bc1209bdbec8b3108a5c6c71
Aurielle Li <89785862+yuiyeyo@users.noreply.github.com>
2024-02-02 16:45:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect, session
 from functools import wraps
+from datetime import datetime
 import pymongo
 
 app = Flask(__name__)
@@ -46,12 +47,42 @@ def dashboard():
 
     budget = db.budgeting.find_one({"users_id": session['user']['_id']})
 
-    return render_template('dashboard.html', user=user, budget=budget)
+    user = db.users.find_one({"_id": session['user']['_id']})
+
+    if not user:
+        return jsonify({"error": "User not found in the database"}), 404
+
+    budget = db.budgeting.find_one({"users_id": session['user']['_id']})
+
+    transactions_data = db.transactions.find({"users_id": session['user']['_id']}).sort("date", -1)
+
+    # Calculate the start date of the current month
+    today = datetime.now()
+    start_date = today.replace(day=1)
+
+    # Filter transactions for the current month
+    transactions_data_current_month = db.transactions.find({
+        "users_id": session['user']['_id'],
+        "date": {"$gte": start_date.strftime('%Y-%m-%d')}
+    }).sort("date", -1)
+
+    # Calculate the total amount of transactions for the current month
+    total_amount_current_month = sum(int(transaction['amount']) for transaction in transactions_data_current_month)
+
+    current_date = today.strftime('%Y-%m-%d')
+
+    return render_template('dashboard.html', user=user, budget=budget, transactions_data=transactions_data, current_date=current_date, transactions_data_current_month=transactions_data_current_month, total_amount_current_month=total_amount_current_month)
+
 
 @app.route('/addincome')
 @login_required
 def addincome():
-    return 'add income'
+    return render_template('addincome.html')
+
+@app.route('/addoutcome')
+@login_required
+def addoutcome():
+    return render_template('addoutcome.html')
 
 @app.route('/user/signup', methods=['POST'])
 def signup_routes():
@@ -70,6 +101,16 @@ def signin_routes():
 def budgeting_routes():
     return budgeting()
 
+@app.route ('/user/income', methods = ['POST'])
+@login_required
+def income_routes():
+    return income()
+
+@app.route ('/user/outcome', methods = ['POST'])
+@login_required
+def outcome_routes():
+    return outcome()
+
 if __name__ == '__main__':
-    from user.routes import signup, signout, signin, budgeting
+    from user.routes import signup, signout, signin, budgeting, income, outcome
     app.run(debug=True)
